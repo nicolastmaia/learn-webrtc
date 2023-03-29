2d5edb2887745caaa34fde65b0fa6878fff484ee
Nicolas Maia <nicolas.maia@neopath.com.br>
2023-03-29 17:08:34 -0300

    use only one App component per page

--- client/index.js
+++ client/index.js
@@ -3,11 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App2';
+import App from './App';
 
 import {name as appName} from './app.json';
 import {registerGlobals} from 'react-native-webrtc';
 
 registerGlobals();
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => () => App({wsPort: 3000}));
